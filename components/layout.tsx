42fd1af6907fe2198f0f4deb16a9ba865c34b925
ak-yas <lolesp295@gmail.com>
2022-02-17 22:23:21 +0000

    layout scaffold

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,9 +3,14 @@ import { Box } from '@chakra-ui/layout'
 const Layout = ({ children }) => {
   return (
     <>
-      <Box>
-        Layout
-        {children}
+      <Box width="100vw" height="100vh">
+        <Box position="absolute" top="0" left="0" width="250px">
+          navigation bar
+        </Box>
+        <Box marginLeft="250px">{children}</Box>
+        <Box position="absolute" bottom="0" left="0">
+          Music platey
+        </Box>
       </Box>
     </>
   )
