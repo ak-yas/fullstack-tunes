93f560ba379f373b78841e086b85a2b20fb9bdcc
ak-yas <lolesp295@gmail.com>
2022-02-17 23:13:26 +0000

    logo

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,17 @@
 import { Box } from '@chakra-ui/layout'
+import Sidebar from './sidebar'
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Box width="100vw" height="100vh">
-        <Box position="absolute" top="0" left="0" width="250px">
-          navigation bar
-        </Box>
-        <Box marginLeft="250px">{children}</Box>
-        <Box position="absolute" bottom="0" left="0">
-          Music platey
-        </Box>
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" left="0" width="250px">
+        <Sidebar />
       </Box>
-    </>
+      <Box marginLeft="250px">{children}</Box>
+      <Box position="absolute" bottom="0" left="0">
+        Music player
+      </Box>
+    </Box>
   )
 }
 
