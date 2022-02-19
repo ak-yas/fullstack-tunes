4a67d54c26d671cbe58bb50283fad937df2804eb
ak-yas <lolesp295@gmail.com>
2022-02-19 00:25:24 +0000

    hooks

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,7 +4,7 @@ import prisma from './prisma'
 
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = req.cookies
+    const token = req.cookies.TUNES_ACCESS_TOKEN
 
     if (token) {
       let user
@@ -12,19 +12,22 @@ export const validateRoute = (handler) => {
       try {
         const { id } = jwt.verify(token, 'hello')
         user = await prisma.user.findUnique({
-          where: {
-            id,
-          },
+          where: { id },
         })
 
         if (!user) {
-          throw new Error('User not recognized')
+          throw new Error('Not real user')
         }
       } catch (error) {
         res.status(401)
-        res.json({ error: 'Not Authorized' })
+        res.json({ error: 'Not Authorizied' })
+        return
       }
+
       return handler(req, res, user)
     }
+
+    res.status(401)
+    res.json({ error: 'Not Authorizied' })
   }
 }
